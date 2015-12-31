0a0a97edefa91bf47ca7d42cf573a2290457721d
Hanafi Ahmat <ahmat.hanafi@gmail.com>
2015-12-31 13:23:06 +0800

    added src folder folder with wordpress files

---
+++ src/AutoloadExample.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace appitnetwork\extended;
+
+/**
+ * This is just an example.
+ */
+class AutoloadExample extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
